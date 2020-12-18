8c3f0e35ad48fe4d4d1ee4eb37e662c42c8022d6
Keith <k.shintaku@gmail.com>
2020-12-18 00:00:15 -0800

    added background to navbar

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -1,10 +1,22 @@
 // Opening the side menu when in tablet/mobile view
 function toggleNav() {
-    if (document.getElementById('nav-icon').className === '') {
-        document.getElementById('nav-icon').className = 'open'
-        document.getElementById('side-nav').style.width = '72%'
+    if (document.getElementById("nav-icon").className === "") {
+        document.getElementById("nav-icon").className = "open";
+        document.getElementById("side-nav").style.width = "72%";
     } else {
-        document.getElementById('nav-icon').className = ''
-        document.getElementById('side-nav').style.width = '0'
+        document.getElementById("nav-icon").className = "";
+        document.getElementById("side-nav").style.width = "0";
     }
 }
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.backgroundColor = "transparent";
+    } else {
+        document.getElementById("navbar").style.backgroundColor = "lightgrey";
+    }
+    prevScrollpos = currentScrollPos;
+};
