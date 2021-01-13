e56c45421d184a6b28fe6cf2c2c6997518e79de9
Keith <k.shintaku@gmail.com>
2021-01-12 21:51:54 -0800

    another new font

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+function toggleFont() {
+    if (document.getElementById("font-test").style.fontFamily === "OleoScript-Regular") {
+        document.getElementById("font-test").style.fontFamily = "Courgette-Regular";
+    } else {
+        document.getElementById("font-test").style.fontFamily = "OleoScript-Regular";
+    }
+}
