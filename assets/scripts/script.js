27309f66f0647466ec69302c87b3a29a95c7e052
Keith <k.shintaku@gmail.com>
2021-02-13 14:27:35 -0800

    ig scraped and put into json

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,25 @@
-function toggleFont() {
-    if (document.getElementById("font-test").style.fontFamily === "OleoScript-Regular") {
-        document.getElementById("font-test").style.fontFamily = "Courgette-Regular";
-    } else {
-        document.getElementById("font-test").style.fontFamily = "OleoScript-Regular";
+fetch("/ig_data.json")
+.then(response => {
+    return response.json();
+})
+.then(data => {
+    var i;
+    for (i = 0; i < data["hairbyami2021"].length; i++) {
+        var a_tag = document.createElement("a");
+        var img_link = document.createElement("img");
+        a_tag.href = "https://www.instagram.com/p/" + data["hairbyami2021"][i]["shortcode"] + "/";
+        img_link.src = data["hairbyami2021"][i]["display_url"];
+        a_tag.appendChild(img_link);
+        document.getElementById("instagram").appendChild(a_tag);
     }
-}
+    
+});
+
+
+// function toggleFont() {
+//     if (document.getElementById("font-test").style.fontFamily === "OleoScript-Regular") {
+//         document.getElementById("font-test").style.fontFamily = "Courgette-Regular";
+//     } else {
+//         document.getElementById("font-test").style.fontFamily = "OleoScript-Regular";
+//     }
+// }
--- ig_scraper.py
+++ ig_scraper.py
@@ -1,11 +1,13 @@
 import requests
 import os
+import json
 from os.path import join, dirname
 from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
+# Getting information from instagram via rapidapi (limit 5 per day)
 url = "https://instagramdimashirokovv1.p.rapidapi.com/user/hairbyami2021"
 
 headers = {
@@ -17,4 +19,15 @@ response = requests.request("GET", url, headers=headers)
 with open("data.txt", "w") as file:
     file.write(response.text)
 
-print(response.text)
+with open("data.txt", "r") as file:
+    data = file.read()
+
+obj = json.loads(data)
+media = []
+for i in range(len(obj["edge_owner_to_timeline_media"]["edges"])):
+    media.append({"shortcode": obj["edge_owner_to_timeline_media"]["edges"][i]["node"]["shortcode"], 
+    "display_url": obj["edge_owner_to_timeline_media"]["edges"][i]["node"]["display_url"]})
+
+json_out = json.dumps({"hairbyami2021": media})
+with open("ig_data.json", "w") as file:
+    file.write(json_out)
